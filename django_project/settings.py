42420b469dffbd6947ac20ca5ef07583a214be6c
harrison <ndegwaharryxon1998@gmail.com>
2019-01-17 15:06:00 +0300

    another commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = gnmail_user
+EMAIL_HOST_USER = gmail_user
 EMAIL_HOST_PASSWORD = gmail_password
